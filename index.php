b8d3eec53b1e551b71bc83c262c3e9ac71011a16
Jeroen Ho <Jeroenho@Live.nl>
2018-12-19 13:37:34 +0100

    Index navbar wip

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-
+    <link href="css/base.css" rel="stylesheet">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -18,8 +18,74 @@
     <![endif]-->
   </head>
   <body>
-    <h1>Hello, world!</h1>
-
+      <div class="wrapper">
+    <div class="navigation">
+<!--       <nav class="navbar navbar-expand-lg navbar-light bg-light nav-stacked">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
+          </ul>
+        </div>
+      </nav>-->
+    <a class="navbar-brand float-left" href="index.php"><img src="images/logoB-01.svg" class="img-responsive" width="150"></a>
+    <nav class="float-left navbar-left navbar-expand-lg navbar-light bg-light nav-stacked">
+        
+    <div class="nav flex-sm-column nav-pills leftnav btn-sm" id="v-pills-tab" role="tablist">
+      <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
+      <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
+      <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
+      <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
+    
+       
+      </div>
+        <nav>
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#" >Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Profile </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Pricing </a>
+            </li>
+        </ul> 
+        
+        <div class="nav-link" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">...</div>
+      <div class="nav-link" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
+      <div class="nav-link" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
+      <div class="nav-link" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
+    </div>
+    </nav>
+    </div>
+<!--        <div class="row float-left" >
+        <div style="background-color: green" class="col-sm-4 col-md-4 col-lg-4">
+            pik
+        </div>
+        <div style="background-color: blue"class="col-sm-4 col-md-4 col-lg-4">
+            jemoder
+        </div>
+        <div style="background-color: red" class="col-sm-4 col-md-4 col-lg-4">
+        </div>-->
+        
+    </div>
+  </div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
