86e76ffec9822f3845666429b06bdd28a9477133
Jeroen Ho <Jeroenho@Live.nl>
2019-01-07 11:39:32 +0100

    sidebar in aparte php

---
+++ sidebar.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/sideBar.css" rel="stylesheet">  
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0/umd/popper.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <!--<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+    <title>I Learn Flix</title>
+</head>
+
+<body>
+<div class="container-fluid">
+    <div class="row d-flex d-md-block flex-nowrap wrapper">
+        <div class="m-0-col-md-2 float-left col-1 pl-0 pr-0 collapse width show logobg"  id="sidebar">
+            <a><img class="img-fluid" id="logo" src="images/logoB-01.svg"></a>
+                <div class="list-group border-0 card text-center text-md-left">
+                    <a href="#" class="list-group-item d-inline-block collapsed" data-toggle="collapse" aria-expanded="false"><i class="fa fa-home"></i> <span class="d-none d-md-inline">Home</span></a>
+                    <a href="#" class="list-group-item d-inline-block collapsed"><i class="fa fa-film"></i> <span class="d-none d-md-inline">MijnFlix</span></a>
+                    <a href="#" class="list-group-item d-inline-block collapsed" data-toggle="collapse" aria-expanded="false"><i class="fa fa-star"></i> <span class="d-none d-md-inline">Favorieten </span></a>
+                    <a href="#" class="list-group-item d-inline-block collapsed" data-parent="#sidebar"><i class="fas fa-registered"></i> <span class="d-none d-md-inline">Registreren</span></a>
+                    <a href="#" class="list-group-item d-inline-block collapsed" data-parent="#sidebar"><i class="fas fa-sign-in-alt"></i> <span class="d-none d-md-inline">Inloggen</span></a>
+                    <div class="list-group border-0 card text-center text-md-left"> 
+                        <div class="row fixed-bottom">
+                            <a href="#" class="list-group-item d-inline-block collapsed" data-toggle="collapse" aria-expanded="false"><i class="fab fa-facebook"></i> <span class="d-none d-md-inline"><img src="images/F_icon.svg" class="img-fluid sicons" alt="facebook"></span></a>
+                        </div>
+                    </div>
+                        <div class="row">
+                     <footer class="page-footer font-small mdb-color lighten-3 pt-4">
+
+                        <!-- Footer Elements -->
+                        <div class="container">
+
+                          <!--Grid row-->
+                          <div class="row">
+
+                            <!--Grid column-->
+                            <div class="col-lg-2 col-md-1 col-sm-1 mb-4 sicons">
+
+                              <!--Image-->
+                              <div class="view overlay z-depth-1-half">
+                                  <img src="images/F_icon.svg" class="img-fluid" alt="facebook">
+                                  <i class="fab fa-facebook"><a class="list-group-itemd-inline-block collapsed" href=""></i>
+                                  <div class="mask rgba-white-light"></div>
+                                </a>
+                              </div>
+
+                            </div>
+                            <!--Grid column-->
+
+                            <!--Grid column-->
+                            <div class="col-lg-2 col-md-1 mb-4 sicons">
+
+                              <!--Image-->
+                              <div class="view overlay z-depth-1-half">
+                                <img src="images/I_icon.svg" class="img-fluid" alt="insta">
+                                <a href="">
+                                  <div class="mask rgba-white-light"></div>
+                                </a>
+                              </div>
+
+                            </div>
+                            <!--Grid column-->
+
+                            <!--Grid column-->
+                            <div class="col-lg-2 col-md-1 mb-4 sicons">
+
+                              <!--Image-->
+                              <div class="view overlay z-depth-1-half">
+                                <img src="images/T_icon.svg" class="img-fluid" alt="twitter">
+                                <a href="">
+                                  <div class="mask rgba-white-light"></div>
+                                </a>
+                              </div>
+
+                            </div>
+                            <!--Grid column-->
+
+                            
+                          </div>
+                          <!--Grid row-->
+
+                        </div>
+                        <!-- Footer Elements -->
+
+                        <!-- Copyright -->
+<!--                        <div class="footer-copyright text-center py-3">� 2018 Copyright:
+                          <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+                        </div>-->
+                        <!-- Copyright -->
+
+                      </footer>
+                </div>
+                    
+<!--                    <div class="col-md-2 socialmedia">
+                            <a><img class="img-fluid" id="logo" src="images/logoB-01.svg"></a>
+                        </div>-->
+                    </div>
+                
+            
+        </div>
+    </div>
+    <a href="#" data-target="#sidebar" data-toggle="collapse"><i class="fa fa-bars fa-2x py-2 p-1"></i></a>
+</div>
+</body>
+
+</html>
