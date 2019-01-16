d9c48117b089e096b385ca7ffe84c992c21359ff
Jeroen Ho <Jeroenho@Live.nl>
2019-01-16 13:16:08 +0100

    Sidebar bijgewerkt

--- sidebar.php
+++ sidebar.php
@@ -18,95 +18,24 @@
 <body>
 <div class="container-fluid">
     <div class="row d-flex d-md-block flex-nowrap wrapper">
-        <div class="m-0-col-md-2 float-left col-1 pl-0 pr-0 collapse width show logobg"  id="sidebar">
+        <div class="col-md-2 col-sm-1 float-left col-1 pl-0 pr-0 collapse width show logobg"  id="sidebar">
             <a><img class="img-fluid" id="logo" src="images/logoB-01.svg"></a>
                 <div class="list-group border-0 card text-center text-md-left">
                     <a href="#" class="list-group-item d-inline-block collapsed" data-toggle="collapse" aria-expanded="false"><i class="fa fa-home"></i> <span class="d-none d-md-inline">Home</span></a>
                     <a href="#" class="list-group-item d-inline-block collapsed"><i class="fa fa-film"></i> <span class="d-none d-md-inline">MijnFlix</span></a>
                     <a href="#" class="list-group-item d-inline-block collapsed" data-toggle="collapse" aria-expanded="false"><i class="fa fa-star"></i> <span class="d-none d-md-inline">Favorieten </span></a>
                     <a href="#" class="list-group-item d-inline-block collapsed" data-parent="#sidebar"><i class="fas fa-registered"></i> <span class="d-none d-md-inline">Registreren</span></a>
-                    <a href="#" class="list-group-item d-inline-block collapsed" data-parent="#sidebar"><i class="fas fa-sign-in-alt"></i> <span class="d-none d-md-inline">Inloggen</span></a>
-                    <div class="list-group border-0 card text-center text-md-left"> 
-                        <div class="row fixed-bottom">
-                            <a href="#" class="list-group-item d-inline-block collapsed" data-toggle="collapse" aria-expanded="false"><i class="fab fa-facebook"></i> <span class="d-none d-md-inline"><img src="images/F_icon.svg" class="img-fluid sicons" alt="facebook"></span></a>
-                        </div>
-                    </div>
-                        <div class="row">
-                     <footer class="page-footer font-small mdb-color lighten-3 pt-4">
-
-                        <!-- Footer Elements -->
-                        <div class="container">
-
-                          <!--Grid row-->
-                          <div class="row">
-
-                            <!--Grid column-->
-                            <div class="col-lg-2 col-md-1 col-sm-1 mb-4 sicons">
-
-                              <!--Image-->
-                              <div class="view overlay z-depth-1-half">
-                                  <img src="images/F_icon.svg" class="img-fluid" alt="facebook">
-                                  <i class="fab fa-facebook"><a class="list-group-itemd-inline-block collapsed" href=""></i>
-                                  <div class="mask rgba-white-light"></div>
-                                </a>
-                              </div>
-
-                            </div>
-                            <!--Grid column-->
-
-                            <!--Grid column-->
-                            <div class="col-lg-2 col-md-1 mb-4 sicons">
-
-                              <!--Image-->
-                              <div class="view overlay z-depth-1-half">
-                                <img src="images/I_icon.svg" class="img-fluid" alt="insta">
-                                <a href="">
-                                  <div class="mask rgba-white-light"></div>
-                                </a>
-                              </div>
-
-                            </div>
-                            <!--Grid column-->
-
-                            <!--Grid column-->
-                            <div class="col-lg-2 col-md-1 mb-4 sicons">
-
-                              <!--Image-->
-                              <div class="view overlay z-depth-1-half">
-                                <img src="images/T_icon.svg" class="img-fluid" alt="twitter">
-                                <a href="">
-                                  <div class="mask rgba-white-light"></div>
-                                </a>
-                              </div>
-
-                            </div>
-                            <!--Grid column-->
-
-                            
-                          </div>
-                          <!--Grid row-->
-
-                        </div>
-                        <!-- Footer Elements -->
-
-                        <!-- Copyright -->
-<!--                        <div class="footer-copyright text-center py-3">� 2018 Copyright:
-                          <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
-                        </div>-->
-                        <!-- Copyright -->
-
-                      </footer>
-                </div>
+                    <a href="#" class="list-group-item d-inline-block collapsed" data-parent="#sidebar"><i class="fas fa-sign-in-alt "></i> <span class="d-none d-md-inline">Inloggen</span></a>
                     
-<!--                    <div class="col-md-2 socialmedia">
-                            <a><img class="img-fluid" id="logo" src="images/logoB-01.svg"></a>
-                        </div>-->
+                    <!--<div class="list-group border-0 card text-center text-md-left">--> 
+                    <div class="row socialbar d-sm-none d-md-block">
+                        <a href="#" class="list-group-item d-inline-block collapsed" data-parent="#sidebar"><i class="col-md-2 col-lg-4 fab fa-facebook sicons"></i><i class="col-md-2 col-lg-4 fab fa-twitter sicons"></i> <i class="col-md-4 col-lg-4 fab fa-instagram sicons"></i> <span class="d-none d-md-inline"></span></a>
                     </div>
+                </div>
                 
             
         </div>
     </div>
-    <a href="#" data-target="#sidebar" data-toggle="collapse"><i class="fa fa-bars fa-2x py-2 p-1"></i></a>
 </div>
 </body>
 
