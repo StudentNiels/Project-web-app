31768345d26bdb9416f7cc21934ba1ef51eaf743
Sytze van der Gaag <43172319+SytzeVDGaag@users.noreply.github.com>
2019-01-16 13:15:03 +0100

    Update TableCreate.sql
    FK gefixt

--- TableCreate.sql
+++ TableCreate.sql
@@ -38,9 +38,9 @@ CREATE TABLE `Video` (
 	Vak VARCHAR(13) NOT NULL,
 	CONSTRAINT pk_video
 		PRIMARY KEY(VideoID),
-	CONSTRAINT fk_school_video
-		FOREIGN KEY(SchoolID)
-		REFERENCES School(SchoolID)
+	CONSTRAINT fk_user_video
+		FOREIGN KEY(userID)
+		REFERENCES user(userID)
 );
 
 CREATE TABLE `DocentKanaal` (
