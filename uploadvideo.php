f777e564877a639be120f7242de642e2efd4e2b2
Sytze van der Gaag <43172319+SytzeVDGaag@users.noreply.github.com>
2019-01-17 12:23:37 +0100

    Update uploadvideo.php
    :crab: errors :crab: zijn :crab: gone :crab:

--- uploadvideo.php
+++ uploadvideo.php
@@ -1,59 +1,69 @@
 <?php
-session_start();
 include('conn.php');
 ?>
 <!DOCTYPE html>
 <html>
     <body>
         <?php
+        session_start();
         if ($_SESSION['loggedin'] == True) {
-            if ($_SESSION['docent'] === 1) {
+            $docentPerms = $_SESSION['docent'];
+            if ($docentPerms === 1) {
                 echo '<form method="post" action="uploadvideo.php" enctype="multipart/form-data">
-            <p>Enter het vak: 
-                <select name="vak">
-                    <option value="php">PHP</option>
-                    <option value="html/css">HTML/CSS</option>
-                    <option value="databases">Databases</option>
-                    <option value="overige">Overige</option>
-                </select></p>
-            <p>Enter de titel: <input type="text" name="Titel_entered"/></p>
-            <input type="file" name"file" accept=".mp4, .avi, .3gp, .mov, .mpeg"/>
-            <p><input type="submit" value="Upload" name="but_upload"></p>
-            </form>';
+                        <p>Enter het vak: 
+                            <select name="vak">
+                                <option value="php">PHP</option>
+                                <option value="html/css">HTML/CSS</option>
+                                <option value="databases">Databases</option>
+                                <option value="overige">Overige</option>
+                            </select></p>
+                        <p>Enter de titel: <input type="text" name="Titel_entered"/></p>
+                        <input type="file" name="file" accept=".mp4, .avi, .3gp, .mov, .mpeg"/>
+                        <p><input type="submit" value="Upload" name="but_upload"></p>
+                        </form>';
 
                 if (isset($_POST['but_upload'])) {
                     if (empty($_POST['Titel_entered']) || empty($_POST['vak'])) {
                         echo "<p>Vul alle velden in</p>";
                     } else {
                         if (isset($_POST['but_upload'])) {
-                            $maxsize = 5242880000;
-                            $userID = $_SESSION['userID'];
+                            $userID = $_SESSION['userId'];
                             $name = $_FILES['file']['name'];
                             $titel = $_POST['Titel_entered'];
                             $vak = $_POST['vak'];
-                            $target_dir = "videos/";
-                            $target_file = $target_dir . $titel . ".mp4";
-                            if (($_FILES['file']['size'] >= $maxsize) || ($_FILES["file"]["size"] == 0)) {
-                                echo "File too large";
+
+                            $target_location = "videos/" . $titel . ".mp4";
+                            if (($_FILES['file']['size'] === 0)) {
+                                echo "Kies een video om up te loaden!";
                             } else {
                                 // Upload
-                                if (move_uploaded_file($_FILES['file']['tmp_name'], $target_file)) {
+                                if (move_uploaded_file($_FILES['file']['tmp_name'], $target_location)) {
                                     // Insert record
-                                    $query = "INSERT INTO video(UserID, Titel, Vak, Locatie) VALUES(" . $userID . "'" . $titel . "','" . $vak . "','" . $target_file . "')";
-
-                                    mysqli_query($conn, $query);
-                                    echo "<h1>Upload successfully.<h1>";
+                                    $query = "INSERT INTO video(UserID, Titel, Vak, Locatie) VALUES( ?, ?, ?, ?)";
+                                    if ($stmt = mysqli_prepare($conn, $query)) {
+                                        mysqli_stmt_bind_param($stmt, 'isss', $userID, $titel, $vak, $target_location);
+                                        $QueryResult = mysqli_stmt_execute($stmt);
+                                        if ($QueryResult === FALSE) {
+                                            echo "<p>Unable to execute the query.</p>"
+                                            . "<p>Error code "
+                                            . mysqli_errno($DBConnect)
+                                            . ": "
+                                            . mysqli_error($DBConnect)
+                                            . "</p>";
+                                        } else {
+                                            echo "<h1>Upload successfully.<h1>";
+                                        }
+                                    }
                                 }
                             }
                         }
                     }
+                } else {
+                    echo "<p>U heeft niet de juiste rechten om deze functie te gebruiken</p>";
                 }
             } else {
-                echo "<p>U heeft niet de juiste rechten om deze functie te gebruiken</p>";
-            } 
-            mysqli_close($DBConnect);
-        } else {
-            header('location: login.php');
+                header('location: inlog.php');
+            }
         }
         ?>
 
