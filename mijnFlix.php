7a2a439df50262bc39744a1bfe0a3cfd906dc233
StudentNiels <43601448+StudentNiels@users.noreply.github.com>
2019-01-18 15:23:45 +0100

    update mijnflix
    koppelingen  maken

--- mijnFlix.php
+++ mijnFlix.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script type="text/javascript" src="js/materialize.min.js"></script>
         <script type="text/javascript" src="js/script.js"></script>
@@ -18,53 +18,37 @@
 
         <?php
         session_start();
-        $DBConnect = mysqli_connect("localhost", "root", "");
-        if ($DBConnect === FALSE) {
-            echo "<p>Unable to connect to the database server.</p>"
-            . "<p>Error code " . mysqli_errno() . ": " . mysqli_error()
-            . "</p>";
-        } else {
-            if (isset($_SESSION['user'])) {
+        include('conn.php');
+        Print_r($_SESSION);
+        $TableName = "user";
+        $stmt1 = "SELECT Email, SchoolID, AbonnementID, Wachtwoord, DocentPerms FROM " . $TableName . "WHERE userId = " . $_SESSION['userId'];
+        if ($stmt2 = mysqli_prepare($conn, $stmt1)) {
+            mysqli_stmt_execute($stmt2);
 
-                $DBName = "ilearnflix";
-                if (!mysqli_select_db($DBConnect, $DBName)) {
-                    echo "<p>error no  data found</p>";
-                } else {
-                    $TableName = "user";
-                    $stmt1 = "SELECT userID, Email, SchoolID, AbonnementID, Wachtwoord, DocentPerms FROM " . $TableName. "WHERE userID = ". $_SESSION['userID'];
-                    if ($stmt2 = mysqli_prepare($DBConnect, $stmt1)) {
-                        mysqli_stmt_execute($stmt2);
+            mysqli_stmt_bind_result($stmt2, $userID, $Email, $AbonnementID, $Wachtwoord, $DocentPerms);
+            mysqli_stmt_store_result($stmt2);
 
-                        mysqli_stmt_bind_result($stmt2, $userID, $Email, $SchoolID, $AbonnementID, $Wachtwoord, $DocentPerms);
-                        mysqli_stmt_store_result($stmt2);
-
-                        if (mysqli_stmt_num_rows($stmt2) == 0) {
-                            echo "<p>no data found for this profile,contact your administration</p>";
-                        } else {
-                            echo "<p>Your profile</p>";
-                            echo "<table width='100%' border='1'>";
-                            echo "<tr><th>Nr.</th><th>ProductName</th><th>Version</th><th>Hardware type</th><th>Operating System</th><th>Frequency</th><th>Proposed solution</th></tr>";
-                            while (mysqli_stmt_fetch($stmt2)) {
-                                echo "<tr>";
-                                echo "<td>" . $userID . "</td>";
-                                echo "<td>" . $Email . "</td>";
-                                echo "<td>" . $SchoolID . "</td>";
-                                echo "<td>" . $AbonnementID . "</td>";
-                                echo "<td>" . $Wachtwoord . "</td>";
-                                echo "<td>" . $DocentPerms . "</td>";
-                                echo "<td><p><input name='mijnFlix.php' type='submit'></p></td>";
-                                echo "</form></tr>";
-                            }
-                            echo "<p><a href=inlog.php>Add a new bug here</a></p>";
-                            
-                        }
-                        
-                    }
-                    mysqli_stmt_close($stmt2);
+            if (mysqli_stmt_num_rows($stmt2) == 0) {
+                echo "<p>Your profile</p>";
+                echo "<table width='100%' border='1'>";
+                echo "<tr><th>Email</th><th>SchoolID</th><th>AbonnementID</th><th>Wachtwoord</th><th>DocentPerms</th></tr>";
+                while (mysqli_stmt_fetch($stmt2)) {
+                    echo "<tr>";
+                    echo "<td>" . $_SESSION['$Email'] . "</td>";
+                    echo "<td>" . $SchoolID . "</td>";
+                    echo "<td>" . $AbonnementID . "</td>";
+                    echo "<td>" . $Wachtwoord . "</td>";
+                    echo "<td>" . $_SESSION['$DocentPerms'] . "</td>";
+                    echo "<td><p><input name='mijnFlix.php' type='submit'></p></td>";
+                    echo "</form></tr>";
                 }
-                mysqli_close($DBConnect);
+                echo "<p><a href=inlog.php>Add a new bug here</a></p>";
             }
+            mysqli_stmt_close($stmt2);
         }
+        mysqli_close($conn);
+
+
         echo '</pre>';
         ?>
 
@@ -73,31 +57,23 @@
         <?php
         echo"<form method='POST'>";
 
-        echo"<p>userID</p>";
+        echo"<p>" . $_SESSION['username'] . "</p>";
 
-        echo"<p>Email</p>";
-
-        echo"<p>SchoolID</p>";
-
-
+        if (isset($_SESSION['docent'])) {
+            echo"<p>" . $_SESSION['docent'] . "</p>";
+        }
         echo"<p>AbonnementID</p>";
-
-
-        echo"<p>Wachtwoord wijzigen</p>";
-
-
-        echo"<p>DocentPerms yes or no</p>";
-
-
+        echo"<p>Wachtwoord wijzigen <a href=mijnFlix.php>edit</a></p>";
+        echo"<p> $_SESSION[$SchoolID] </p>";
         echo"<p><input type='submit' value='Submit' /></p>";
         echo"</form>";
         echo"<p><a href='index.php'>Terug</a></p>";
-        
+
         if ($_SESSION['docent'] === 1) {
-                                echo "<p><a href=uploadvideo.php>Upload a video</a></p>";
-                            } else {
-                                echo"Indien u een leraar bent en u graag eene video wil uploaden contacteer dan de administratie via admin@email.com";
-                            }
+            echo "<p><a href=uploadvideo.php>Upload a video</a></p>";
+        } else {
+            echo"Indien u een leraar bent en u graag een video wil uploaden contacteer dan de administratie via admin@email.com";
+        }
         ?>
 
 
