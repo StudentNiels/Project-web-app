09e6951a0c2f0671b1f5f1aa80794001937fc1e1
Matta <pietertjepost123@gmail.com>
2019-01-24 13:18:51 +0100

    mergen fix terugzetten

--- betaalverwerking.php
+++ betaalverwerking.php
@@ -20,43 +20,28 @@
                             session_start();
                             include 'conn.php';
                             $_SESSION['betaald'] = '1';
-                            echo '<h1> Bedankt voor de betaling, u word nu omgeleid naar de inlog pagina.</h1>';
-                            echo "<meta http-equiv=\"refresh\" content=\"5;url=inlog.php\"/>";
-                            $betaald = $_SESSION['betaald'];
-                            if($betaald == 1){
-                                $sql = "INSERT INTO abonnement (UserID, BetaalDatum, EindDatum) VALUES( ? , CURDATE(), curdate() + INTERVAL 1 YEAR)";
-                                if($stmt = mysqli_prepare($conn, $sql)){
-                                    mysqli_stmt_bind_param($stmt, "i", $param_userid);
-                                    $param_userid = 2;/*$_SESSION['userId']*/
-                                    if(mysqli_stmt_execute($stmt)){
-                                        
-                                    } else {
-                                        echo "Something went wrong.";
-                                    }
-                                }
-                            }
-                            ?>
-                            <script language="JavaScript">
-                                window.onload = function () {
+                            $userid_query = 'SELECT UserID FROM user WHERE Email = ?';
+                           if($id_stmt = mysqli_prepare($conn, $userid_query)){
+                             mysqli_stmt_bind_param($id_stmt, 's', $_SESSION['temp_betaling']);
+                             if(mysqli_stmt_execute($id_stmt)){
+                               mysqli_stmt_store_result($id_stmt);
+                               mysqli_stmt_bind_result($id_stmt, $userid);
+                               mysqli_stmt_fetch($id_stmt);
+                             } else {
+                                 echo "Something went wrong.";
+                             }
+                             if($betaald == 1){
+                                 $sql = "INSERT INTO abonnement (UserID, BetaalDatum, EindDatum) VALUES( ? , CURDATE(), curdate() + INTERVAL 1 YEAR)";
+                                 if($stmt = mysqli_prepare($conn, $sql)){
+                                   $param_userid = $userid;
+                                     mysqli_stmt_bind_param($stmt, "i", $param_userid);
+                                     if(mysqli_stmt_execute($stmt)){
 
-                                    (function () {
-                                        var counter = 5;
-
-                                        setInterval(function () {
-                                            counter--;
-                                            if (counter >= 0) {
-                                                span = document.getElementById("count");
-                                                span.innerHTML = counter;
-                                            }
-                                            // Display 'counter' wherever you want to display it.
-                                            if (counter === 0) {
-                                                clearInterval(counter);
-                                            }
-
-                                        }, 1000);
-
-                                    })();
-
+                                     } else {
+                                         echo "Something went wrong.";
+                                     }
+                                 }
+                             }
                                 }
                             </script>
                         </div>
@@ -67,4 +52,3 @@
         </div>
     </body>
 </html>
-
