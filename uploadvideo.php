e2874c0a6a6eae14a36db9fcd29abe3f2d03bc0a
Hak <simon.van.der.hak@student.nhlstenden.com>
2019-01-24 14:25:39 +0100

    background fix, andere pagina's

--- uploadvideo.php
+++ uploadvideo.php
@@ -4,7 +4,22 @@ include ('sidebar.php');
 ?>
 <!DOCTYPE html>
 <html>
-    <body>
+ <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/sideBar.css" rel="stylesheet">
+        <link href="css/videoshow.css" rel="stylesheet">
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0/umd/popper.min.js"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+        <!--<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">-->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+         <link href="css/style.css" rel="stylesheet">
+	   <title>I Learn Flix</title>
+    </head>
+    <body class="bcolor">
         <?php
         if ($_SESSION['loggedin'] == True) {
             $docentPerms = $_SESSION['docent'];
