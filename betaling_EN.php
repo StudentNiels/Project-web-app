29055ae2644b698d00b011e3ad62d7d70ef10241
RickdeVries2001 <43599285+RickdeVries2001@users.noreply.github.com>
2019-01-24 20:04:00 +0100

    Create betaling_EN.php

---
+++ betaling_EN.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="styletest.css" rel="stylesheet">
+        <title>Payment</title>
+    </head>
+    <body>
+        <div class="background">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-sm-12 col-md-9 col-lg-7 mt-auto">
+                        <div class="pic-center">
+                            <a><img class="img-fluid"  id="logo" src="images/logoB-01.svg" alt='logo van i learn flix'></a>
+
+                        </div>
+                        <hr>
+                        <div class="text-center col-6 mx-auto">
+                            <form>
+                                <div class="form-group">
+                                    <h3>Get a subscription</h3>
+                                    <a href="betaalverwerking.php"><img src="https://www.paypalobjects.com/webstatic/en_US/i/buttons/checkout-logo-large.png" alt="Check out with PayPal" /></a>                                </div>
+                            </form>
+                            <?php
+                            ?>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </body>
+</html>
