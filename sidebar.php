eec200f5ec5b9035ff727e80cdc1f3da07447d39
StudentNiels <43601448+StudentNiels@users.noreply.github.com>
2019-01-24 20:56:13 +0100

    update sidebar, searchbar

--- sidebar.php
+++ sidebar.php
@@ -17,34 +17,27 @@ include('logcheck.php');
         <title>I Learn Flix</title>
     </head>
 
-    <div class="container-fluid">
+        <div class="container-fluid">
         <div class="row d-flex d-md-block flex-nowrap wrapper">
-            <div class="col-md-2 col-sm-3 col-lg-3 float-left col-3 pl-0 pr-0 collapse width show logobg"  id="sidebar">
-                <a href="index.php"><img class="img-fluid" id="logo" src="images/logoB-01.svg"></a>
+            <div class="col-md-2 col-sm-2 col-lg-2 float-left col-2 pl-0 pr-0 collapse width show logobg" id="sidebar">
+                <img class="img-fluid" id="logo" src="images/logoB-01.svg">
                 <div class="list-group border-0 card text-center text-md-left">
                     <a href="index.php" class="list-group-item d-inline-block collapsed uwu"><i class="fa fa-home"></i> <span class="d-none d-md-inline">Home</span></a>
                     <a href="mijnFlix.php" class="list-group-item d-inline-block collapsed uwu"><i class="fa fa-film"></i> <span class="d-none d-md-inline">MijnFlix</span></a>
                     <a href="loguit.php" class="list-group-item d-inline-block collapsed uwu" data-parent="#sidebar"><i class="fas fa-sign-out-alt"></i> <span class="d-none d-md-inline">Uitloggen</span></a>
                     <form method="post" action="search.php">
-                        <div class="form-group list-group-item d-inline-block collapsed">
-                            <input class="form-control" type="text" name="searchstr" placeholder="" required>
-                        </div>
-                        <div class="form-group list-group-item d-inline-block collapsed">
-                            <input class="form-control" type="submit" name="search" value="Search">
-                        </div>
+                        <i class="offset-sm-1 fas fa-search"></i><span class="d-none d-md-inline"><p class="list-group-item d-inline-block collapsed"><input class="form-control" type="text" name="searchstr" placeholder=""></p></span>
                     </form>
                     <!--<div class="list-group border-0 card text-center text-md-left">-->
-                    <div class="row socialbar d-sm-none d-md-block">
-                        <div class="col-sm-12 col-md-12 col-lg-12 offset-sm-1">
+                    <div class="row socialbar d-sm-n2one d-md-block list-group-item d-inline-block collapsed">
+                        <div class="col-sm-11 col-md-11 col-lg-11 offset-sm-2">
                          <!--<a href="#" class="list-group-item d-inline-block collapsed" data-parent="#sidebar"><i class="col-auto fab fa-facebook sicons"></i><i class="col-md-2 col-lg-4 fab fa-twitter sicons"></i> <i class="col-md-4 col-lg-4 fab fa-instagram sicons"></i> <span class="d-none d-md-inline"></span></a>-->
-                            <a href="https://www.facebook.com" target="_BLANK" class="list-group-item d-inline-block collapsed sicons" data-parent="#sidebar"><i class="col-12 fab fa-facebook"></i><span class="d-none d-lg-inline"></span></a>
-                            <a href="https://www.twitter.com" target="_BLANK" class="list-group-item d-inline-block collapsed sicons" data-parent="#sidebar"><i class="col-12 fab fa-twitter"></i><span class="d-none d-lg-inline"></span></a>
-                            <a href="https://www.instagram.com" target="_BLANK" class="list-group-item d-inline-block collapsed sicons" data-parent="#sidebar"><i class="col-12 fab fa-instagram"></i><span class="d-none d-lg-inline"></span></a>
+                            <a href="https://www.facebook.com" target="_BLANK" class="list-group-item d-inline-block collapsed sicons" data-parent="#sidebar"><i class="col-4 fab fa-facebook"></i><span class="d-none d-lg-inline"></span></a>
+                            <a href="https://www.twitter.com" target="_BLANK" class="list-group-item d-inline-block collapsed sicons" data-parent="#sidebar"><i class="col-4 fab fa-twitter"></i><span class="d-none d-lg-inline"></span></a>
+                            <a href="https://www.instagram.com" target="_BLANK" class="list-group-item d-inline-block collapsed sicons" data-parent="#sidebar"><i class="col-4 fab fa-instagram"></i><span class="d-none d-lg-inline"></span></a>
                         </div>
                     </div>
                 </div>
-
-
             </div>
         </div>
     </div>
