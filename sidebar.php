12f253826d53c5d6865e9127811774c287c8baaa
StudentNiels <43601448+StudentNiels@users.noreply.github.com>
2019-01-25 12:29:23 +0100

    search in sidebar update, searchpage toegevoegd

--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,6 @@
 include('logcheck.php');
 ?>
 <html>
-
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -20,20 +19,15 @@ include('logcheck.php');
     <div class="container-fluid">
         <div class="row d-flex d-md-block flex-nowrap wrapper">
             <div class="col-md-2 col-sm-2 col-xs-1 col-lg-2 float-left col-2 pl-0 pr-0 collapse width show logobg" id="sidebar">
-                <img class="img-fluid" id="logo" src="images/logoB-01.svg">
+                <img class="img-fluid" id="logo" src="images/logoB-01.svg">         
                 <div class="list-group border-0 card text-center text-md-left">
-
-                    <form class="form-inline list-group-item d-inline-block collapsed">
-                        <i class="fas fa-search" aria-hidden="true"></i>
-                        <input class="form-control- form-control-sm ml-3 w-50 d-sm-none d-md-inline"  type="text" aria-label="Search">
-                    </form>
+                    <a href="searchpage.php" class="form-inline list-group-item d-inline-block collapsed"><i class="fas fa-search uwu" aria-hidden="true"></i>
                     <a href="index.php" class="list-group-item d-inline-block collapsed uwu"><i class="fa fa-home"></i> <span class="d-none d-md-inline">Home</span></a>
                     <a href="mijnFlix.php" class="list-group-item d-inline-block collapsed uwu"><i class="fa fa-film"></i> <span class="d-none d-md-inline">MijnFlix</span></a>
                     <a href="favorieten.php" class="list-group-item d-inline-block collapsed uwu"><i class="fa fa-asterisk"></i> <span class="d-none d-md-inline">Favorieten</span></a>
                     <a href="index_EN.php" class="list-group-item d-inline-block collapsed uwu"data-parent="#sidebar"><i class="fas fa-flag"></i> <span class="d-none d-md-inline">English</span></a>
                     <a href="loguit.php" class="list-group-item d-inline-block collapsed uwu" data-parent="#sidebar"><i class="fas fa-sign-out-alt"></i> <span class="d-none d-md-inline">Uitloggen</span></a>
-
-
+                    
                     <div class="col-sm-11 col-md-11 col-lg-11 offset-sm-2 socialbar">
                      <!--<a href="#" class="list-group-item d-inline-block collapsed" data-parent="#sidebar"><i class="col-auto fab fa-facebook sicons"></i><i class="col-md-2 col-lg-4 fab fa-twitter sicons"></i> <i class="col-md-4 col-lg-4 fab fa-instagram sicons"></i> <span class="d-none d-md-inline"></span></a>-->
                         <a href="https://www.facebook.com" target="_BLANK" class="list-group-item d-inline-block collapsed siconwrap" data-parent="#sidebar"><i class="col-4 fab fa-facebook sicons"></i><span class="d-none d-lg-inline"></span></a>
